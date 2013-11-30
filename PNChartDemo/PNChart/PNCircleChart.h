2ca4575dbea34b4f4cb3499ea8b30b5097e22847
kevin <kevin@ifanr.com>
2013-11-30 11:13:52 +0800

    Add PNCircleChart

---
+++ PNChartDemo/PNChart/PNCircleChart.h
@@ -0,0 +1,13 @@
+//
+//  PNCircleChart.h
+//  PNChartDemo
+//
+//  Created by kevinzhow on 13-11-30.
+//  Copyright (c) 2013年 kevinzhow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PNCircleChart : UIView
+
+@end
---
+++ PNChartDemo/PNChart/PNCircleChart.m
@@ -0,0 +1,31 @@
+//
+//  PNCircleChart.m
+//  PNChartDemo
+//
+//  Created by kevinzhow on 13-11-30.
+//  Copyright (c) 2013年 kevinzhow. All rights reserved.
+//
+
+#import "PNCircleChart.h"
+
+@implementation PNCircleChart
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
