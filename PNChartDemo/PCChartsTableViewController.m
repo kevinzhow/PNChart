8c02df9496f5901b21de193a04db3e4d8d3cb5c9
kevinzhow <kevinchou.c@gmail.com>
2013-12-17 09:32:51 +0800

    Update example

--- PNChartDemo/PCChartsTableViewController.m
+++ PNChartDemo/PCChartsTableViewController.m
@@ -65,21 +65,23 @@
         [lineChart setXLabels:@[@"SEP 1",@"SEP 2",@"SEP 3",@"SEP 4",@"SEP 5",@"SEP 6",@"SEP 7"]];
 
         // Line Chart Nr.1
+        NSArray * data01Array = @[@60.1, @160.1, @126.4, @262.2, @186.2, @127.2, @176.2];
         PNLineChartData *data01 = [PNLineChartData new];
         data01.color = PNFreshGreen;
         data01.itemCount = lineChart.xLabels.count;
-        data01.getData = ^(NSUInteger item) {
-            CGFloat y = item * 10;
-            return [PNLineChartDataItem dataItemWithY:y];
+        data01.getData = ^(NSUInteger index) {
+            CGFloat yValue = [[data01Array objectAtIndex:index] floatValue];
+            return [PNLineChartDataItem dataItemWithY:yValue];
         };
 
         // Line Chart Nr.2
+        NSArray * data02Array = @[@20.1, @180.1, @26.4, @202.2, @126.2, @167.2, @276.2];
         PNLineChartData *data02 = [PNLineChartData new];
         data02.color = PNTwitterColor;
         data02.itemCount = lineChart.xLabels.count;
-        data02.getData = ^(NSUInteger item) {
-            CGFloat y = item == 0 ? (item * 5) + 10 : (item * 5);
-            return [PNLineChartDataItem dataItemWithY:y];
+        data02.getData = ^(NSUInteger index) {
+            CGFloat yValue = [[data02Array objectAtIndex:index] floatValue];
+            return [PNLineChartDataItem dataItemWithY:yValue];
         };
 
         lineChart.chartData = @[data01, data02];
