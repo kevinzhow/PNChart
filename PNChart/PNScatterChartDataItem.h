a675504ecc3a28fa2732b7de449ad0316b5c84fa
MrWooJ <hurricanc@gmail.com>
2014-12-04 11:14:29 +0330

    Adding PNScatterChart - DataItem

---
+++ PNChart/PNScatterChartDataItem.h
@@ -0,0 +1,18 @@
+//
+//  PNScatterChartDataItem.h
+//  PNChartDemo
+//
+//  Created by Alireza Arabi on 12/4/14.
+//  Copyright (c) 2014 kevinzhow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PNScatterChartDataItem : NSObject
+
++ (PNScatterChartDataItem *)dataItemWithX:(CGFloat)x AndWithY:(CGFloat)y;
+
+@property (readonly) CGFloat x; // should be within the x range
+@property (readonly) CGFloat y; // should be within the y range
+
+@end
