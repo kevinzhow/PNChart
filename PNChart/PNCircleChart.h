6f25a92f5b33c056d9dcb085a9b712bf0ec57b6b
Craig McLeod <Craig.McLeod@activenetwork.com>
2015-01-01 22:30:42 -0700

    Added additional initWithFrame methods to allow for choice of shadowColor, displayCountingLabel BOOL and overrideLineWidth option.
    Also change the UIBezierPath radius calculation so circle uses the full frame space allotted.

--- PNChart/PNCircleChart.h
+++ PNChart/PNCircleChart.h
@@ -29,6 +29,30 @@ typedef NS_ENUM (NSUInteger, PNChartFormatType) {
           clockwise:(BOOL)clockwise
              shadow:(BOOL)hasBackgroundShadow;
 
+- (id)initWithFrame:(CGRect)frame
+              total:(NSNumber *)total
+            current:(NSNumber *)current
+          clockwise:(BOOL)clockwise
+             shadow:(BOOL)hasBackgroundShadow
+        shadowColor:(UIColor *)backgroundShadowColor;
+
+- (id)initWithFrame:(CGRect)frame
+              total:(NSNumber *)total
+            current:(NSNumber *)current
+          clockwise:(BOOL)clockwise
+             shadow:(BOOL)hasBackgroundShadow
+        shadowColor:(UIColor *)backgroundShadowColor
+displayCountingLabel:(BOOL)displayCountingLabel;
+
+- (id)initWithFrame:(CGRect)frame
+              total:(NSNumber *)total
+            current:(NSNumber *)current
+          clockwise:(BOOL)clockwise
+             shadow:(BOOL)hasBackgroundShadow
+        shadowColor:(UIColor *)backgroundShadowColor
+displayCountingLabel:(BOOL)displayCountingLabel
+  overrideLineWidth:(NSNumber *)overrideLineWidth;
+
 @property (strong, nonatomic) UICountingLabel *countingLabel;
 @property (nonatomic) UIColor *strokeColor;
 @property (nonatomic) UIColor *strokeColorGradientStart;
@@ -42,5 +66,7 @@ typedef NS_ENUM (NSUInteger, PNChartFormatType) {
 @property (nonatomic) CAShapeLayer *circle;
 @property (nonatomic) CAShapeLayer *gradientMask;
 @property (nonatomic) CAShapeLayer *circleBackground;
+
+@property (nonatomic) BOOL displayCountingLabel;
 
 @end
